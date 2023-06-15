6560acfbf1df7a17f998fca283c91da34ac73ae1
Meet Dholariya <129361022+dholariyameet@users.noreply.github.com>
2023-06-15 10:14:37 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+    $('#banner').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:1
+            }
+        }
+    })
+});
+$(document).ready(function(){
+    $('#brand').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        autoplay:true,
+        autoplayTimeout:2000,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:4
+            }
+        }
+    })
+});
+AOS.init();
+setTimeout(function(){
+    $('.pre-loader').fadeOut();
+},3000)
